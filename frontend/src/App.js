6e7bff4a99832d1ca84f3040932e0bd0eb48c19c
EinarLop <36113521+Einar-Lop@users.noreply.github.com>
2020-11-10 13:29:00 -0600

    Header + Product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-
+import Header from "./GlobalComponents/header"
+import Product from "./Inventory/product"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <Header color="yellow" />
+      <Product />
 
     </div>
   );
