fabc262d1c525b3762c1d540b9b2e40a11ea75aa
EinarLop <36113521+Einar-Lop@users.noreply.github.com>
2020-11-21 23:45:31 -0600

    Header and Searchbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-//import Header from "./GlobalComponents/Header"
+import Header from "./GlobalComponents/Header"
 import Product from "./Inventory/Components/Product"
-//import SearchBar from "./GlobalComponents/SearchBar"
+import SearchBar from "./GlobalComponents/SearchBar"
 import './App.css';
 import ShowProducts from "./Inventory/ShowProducts";
 import CreateProduct from "./Inventory/CreateProduct"
@@ -9,12 +9,12 @@ function App() {
   return (
     <div className="App">
     
-    {/* <Header color="yellow" /> */}
+    <Header color="yellow" />
 
       <CreateProduct/>
       <ShowProducts/>
       
-      {/* <SearchBar/> */}
+      <SearchBar/>
       <Product />
       <Product />
       
