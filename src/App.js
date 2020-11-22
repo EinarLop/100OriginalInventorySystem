dbcfc0cb06e6d793e78d3f989e9a8ec618ee160c
EinarLop <36113521+Einar-Lop@users.noreply.github.com>
2020-11-22 13:31:04 -0600

    Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import Header from "./GlobalComponents/Header"
-import Product from "./Inventory/Components/Product"
+import Product from "./Inventory/Components/Product";
 import SearchBar from "./GlobalComponents/SearchBar"
 import './App.css';
 import ShowProducts from "./Inventory/ShowProducts";
 import CreateProduct from "./Inventory/CreateProduct"
+import CreateSale from "./Sale/CreateSale";
+import ShowSales from "./Sale/ShowSales";
+import CreateSupplier from "./Supplier/CreateSupplier"
+import ShowSuppliers from "./Supplier/ShowSupplier"
+
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-    
-    <Header color="yellow" />
 
-      <CreateProduct/>
-      <ShowProducts/>
+  
+   
+    <Router>
+        <Header color="yellow" />
+        {/* <SearchBar/> */}
+      <Switch>
+         <Route exact path="/createproduct" component={CreateProduct}/>
+         <Route exact path="/showproducts" component={ShowProducts}/>
+         <Route exact path="/createsale" component={CreateSale}/>
+         <Route exact path="/showsales" component={ShowSales}/>
+         <Route exact path="/createsupplier" component={CreateSupplier}/>
+         <Route exact path="/showsuppliers" component={ShowSuppliers}/>
+
+      </Switch>
+    </Router>
+ 
+
+
+     
       
-      <SearchBar/>
-      <Product />
-      <Product />
-      
+   
 
 
     </div>
