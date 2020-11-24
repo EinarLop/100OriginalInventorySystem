4023f6927fbe99f6a43d956df4b5819e20ae2471
EricJ <ericjardon@hotmail.com>
2020-11-24 17:23:06 -0600

    CreateSale interface and input validation functionality finished. Cross-Ref Quantities saved to DB fixed.

--- src/Inventory/Components/Product.js
+++ src/Inventory/Components/Product.js
@@ -5,7 +5,7 @@ import styles from './ProductStyles.module.scss';
 function Product(props) {
     return (
         <div className={styles.wrapper}>
-            <img className={styles.image} alt="Watch"src="https://cdn.shopify.com/s/files/1/0258/3088/3407/products/AR1737_01.jpg?v=1589472110" />
+            <img className={styles.image} alt="Watch"src={props.url} />
 
             <h2 className={styles.productCode} >{props.productCode}</h2>
             <div className={styles.unitPriceWrapper}>
